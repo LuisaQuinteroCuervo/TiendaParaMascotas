785d1e8a9b0bc0e0fd6a1c656cc9010443090dfd
Cristian Leal <cristianmape@live.com>
2024-11-10 01:54:56 -0500

    backend completo

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql2");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
 
 app.use(cors());
 app.use(express.json());
@@ -11,105 +12,300 @@ const db = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "admin",
-  database: "biblioteca",
+  database: "tiendamascotas",
 });
 
-// Ver todos los prestamos //YA ESTAAAAAAAAAAAAA
-app.get("/prestamos", (req, res) => {
-  const sqlSelect = "SELECT * FROM VistaPrestamos";
+function verificarOCrearAdmin() {
+  const sqlSelect = "SELECT * FROM Usuarios WHERE rol = 'administrador' LIMIT 1";
+  
+  //ver si ya existe un administrador
   db.query(sqlSelect, (err, result) => {
-    if (err) console.log(err);
-    else res.send(result);
-  });
-});
-
-
-
-
-// Ver el historial de los prestamos //YA ESTAAAAAAAAAAAAAAAAAAA
-app.get("/historial", (req, res) => {
-  const sqlSelect = "SELECT * FROM VistaHistorial";
-  db.query(sqlSelect, (err, result) => {
-      if (err) console.log(err);
-      else res.send(result);
-  });
-});
-
-// Ver el Libros /// YA ESTAAAAAAAAAAA
-app.get("/Libros", (req, res) => {
-  const sqlSelect = "SELECT * FROM Libro";
-  db.query(sqlSelect, (err, result) => {
-    if (err) console.log(err);
-    else res.send(result);
-  });
-});
-
-// Registrar prestamo //YA ESTAAAAA
-app.post("/addPrestamo", (req, res) => {
-  const { cedula, nombreCompleto, carrera, correo, numero, tituloLibro } =
-    req.body;
-  const sqlInsert = "CALL RegistrarPrestamo(?,?,?,?,?,?);";
-  db.query(
-    sqlInsert,
-    [cedula, nombreCompleto, carrera, correo, numero, tituloLibro],
-    (err, result) => {
-      if (err) console.log(err);
-      else res.send("Prestamo registrado");
+    if (err) {
+      console.log("Error al verificar el administrador:", err);
+    } else if (result.length === 0) {
+      //crear uno con contraseña encriptada
+      const adminPassword = "admin123";
+      
+      bcrypt.hash(adminPassword, 10, (err, hashedPassword) => {
+        if (err) {
+          console.log("Error al encriptar la contraseña del administrador:", err);
+        } else {
+          const sqlInsert = "CALL CrearAdmin(?, ?, ?)";
+          const nombreAdmin = "admin";
+          const emailAdmin = "adminUser";
+          
+          db.query(sqlInsert, [nombreAdmin, emailAdmin, hashedPassword], (err, result) => {
+            if (err) {
+              console.log("Error al crear el administrador:", err);
+            } else {
+              console.log("Usuario administrador creado con éxito");
+            }
+          });
+        }
+      });
+    } else {
+      console.log("Administrador ya existente");
     }
-  );
-});
-
-// Registrar libro 4 //YA ESTAAAAAA
-app.post("/addLibro", (req, res) => {
-  const { tituloLibro, editorial, autor, fecha, campoEstudio, cantidad } =
-    req.body;
-  const sqlInsert = "CALL InsertarLibro(?,?,?,?,?,?);";
-  db.query(
-    sqlInsert,
-    [tituloLibro, editorial, autor, fecha, campoEstudio, cantidad],
-    (err, result) => {
-      if (err) console.log(err);
-      else res.send("Libro registrado");
-    }
-  );
-});
-
-// Actualizar estado del prestamo (devolucion del libro)
-app.post("/actualizarEstado", (req, res) => {
-  const { idPrestamo } = req.body; // el id del registro del prestamo
-  const sqlInsert = "CALL ActualizarEstado(?);";
-  db.query(sqlInsert, [idPrestamo], (err, result) => {
-    if (err) console.log(err);
-    else res.send("estado del prestamo actualizado: " + idPrestamo);
   });
-});
+}
 
-// Ver el historial de los prestamos
-app.get("/historial", (req, res) => {
-  const sqlSelect = "SELECT * FROM VistaHistorial";
-  db.query(sqlSelect, (err, result) => {
-    if (err) console.log(err);
-    else res.send(result);
-  });
-});
+// Crear usurario (solo crea Clientes)
+app.post("/addUsuario", (req, res) => {
+  const { nombre, email, password } = req.body;
+  const sqlInsert = "CALL CrearUsuario(?,?,?);";
 
-// Validar usuario (inicio de sesion) YaA ESTAAAAAAAAAAAA
-app.post("/validarUsuario", (req, res) => {
-  const { nombreUsuario, contrasena } = req.body;
-  const sqlInsert = "CALL ValidateUser(?,?);";
-  db.query(sqlInsert, [nombreUsuario, contrasena], (err, result) => {
+  //encriptar la contraseña antes de guardarla
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
     if (err) {
       console.log(err);
+      res.status(500).send("Error en la encriptación de la contraseña");
     } else {
-      // Obtener el resultado del procedimiento almacenado
-      const resultado = result[0][0].Resultado;
-      if (resultado) {
-        res.send("usuario validado");
-      } else {
-        res.send("usuario no validado");
-      }
+      db.query(sqlInsert, [nombre, email, hashedPassword], (err, result) => {
+        if (err) console.log(err);
+        else res.send("Usuario registrado");
+      });
     }
   });
+});
+
+// loggin
+app.post("/validarUsuario", (req, res) => {
+  const { email, password } = req.body;
+  
+  //traer la contraseña encriptada del usuario 
+  const sqlSelect = "SELECT password FROM Usuarios WHERE email = ?";
+  
+  db.query(sqlSelect, [email], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error en la validación del usuario");
+    } else if (result.length > 0) {
+      const hashedPassword = result[0].password;
+
+      //comparar la contraseña ingresada con el hash almacenado
+      bcrypt.compare(password, hashedPassword, (err, isMatch) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send("Error en la validación de la contraseña");
+        } else if (isMatch) {
+          const sqlCall = "SELECT ValidarUsuario(?, ?) AS rol;";
+          
+          db.query(sqlCall, [email, hashedPassword], (err, result) => {
+            if (err) {
+              console.log(err);
+              res.status(500).send("Error al obtener el rol del usuario");
+            } else {
+              const rol = result[0].rol;
+              if (rol) res.send(`Usuario validado como ${rol}`);
+              else res.send("Usuario no validado");
+            }
+          });
+        } else {
+          res.send("Contraseña incorrecta");
+        }
+      });
+    } else {
+      res.send("Usuario no encontrado");
+    }
+  });
+});
+
+
+// ver pedidos administrador
+app.get("/pedidos", (req, res) => {
+  const sqlSelect = "SELECT * FROM VistaPedidos";
+  db.query(sqlSelect, (err, result) => {
+    if (err) console.log(err);
+    else res.send(result);
+  });
+});
+
+// editar estado del pedidos
+app.post("/editarEstadoPedido", (req, res) => {
+  const { pedidoId, nuevoEstado } = req.body;
+  const sqlUpdate = "CALL EditarEstadoPedido(?, ?);";
+  db.query(sqlUpdate, [pedidoId, nuevoEstado], (err, result) => {
+    if (err) console.log(err);
+    else res.send(`Estado del pedido ${pedidoId} actualizado a ${nuevoEstado}`);
+  });
+});
+
+//ver pedidos usuarios
+app.get("/pedidosUsuario/:usuarioId", (req, res) => {
+  const usuarioId = req.params.usuarioId;
+  const sqlSelect = "CALL ObtenerPedidosUsuario(?);";
+  db.query(sqlSelect, [usuarioId], (err, result) => {
+    if (err) console.log(err);
+    else res.send(result[0]);
+  });
+});
+
+// crear reservas
+app.post("/addReserva", (req, res) => {
+  const { usuarioId, servicioId, fecha, hora } = req.body;
+  const sqlInsert = "CALL CrearReserva(?, ?, ?, ?);";
+  db.query(sqlInsert, [usuarioId, servicioId, fecha, hora], (err, result) => {
+    if (err) console.log(err);
+    else res.send("Reserva registrada");
+  });
+});
+
+// ver reservas administradores
+app.get("/reservas", (req, res) => {
+  const sqlSelect = "SELECT * FROM VistaReservas";
+  db.query(sqlSelect, (err, result) => {
+    if (err) console.log(err);
+    else res.send(result);
+  });
+});
+
+// ver reservas usuarios
+app.get("/reservasUsuario/:usuarioId", (req, res) => {
+  const usuarioId = req.params.usuarioId;
+  const sqlSelect = "CALL ObtenerReservasUsuario(?);";
+  db.query(sqlSelect, [usuarioId], (err, result) => {
+    if (err) console.log(err);
+    else res.send(result[0]);
+  });
+});
+
+// ver productos administradores
+app.get("/productos", (req, res) => {
+  const sqlSelect = "SELECT * FROM ListaProductos";
+  db.query(sqlSelect, (err, result) => {
+    if (err) console.log(err);
+    else res.send(result);
+  });
+});
+
+//crear producto
+app.post("/addProducto", (req, res) => {
+  const { nombre, descripcion, categoria, precio, imagenUrl, stock } = req.body;
+  const sqlInsert = "CALL CrearProducto(?, ?, ?, ?, ?, ?);";
+  db.query(sqlInsert, [nombre, descripcion, categoria, precio, imagenUrl, stock], (err, result) => {
+    if (err) console.log(err);
+    else res.send("Producto registrado");
+  });
+});
+
+//editar producto
+app.post("/modificarProducto", (req, res) => {
+  const { id, nombre, descripcion, categoria, precio, imagenUrl, stock } = req.body;
+  const sqlUpdate = "CALL ModificarProducto(?, ?, ?, ?, ?, ?, ?);";
+  db.query(sqlUpdate, [id, nombre, descripcion, categoria, precio, imagenUrl, stock], (err, result) => {
+    if (err) console.log(err);
+    else res.send("Producto modificado");
+  });
+});
+
+// eliminar producto
+app.delete("/eliminarProducto/:id", (req, res) => {
+  const id = req.params.id;
+  const sqlDelete = "CALL EliminarProducto(?);";
+  db.query(sqlDelete, [id], (err, result) => {
+    if (err) console.log(err);
+    else res.send("Producto eliminado");
+  });
+});
+
+// ver solo un producto
+app.get("/producto/:id", (req, res) => {
+  const id = req.params.id;
+  const sqlSelect = "CALL ObtenerProductoPorID(?);";
+  db.query(sqlSelect, [id], (err, result) => {
+    if (err) console.log(err);
+    else res.send(result[0][0]);
+  });
+});
+
+//ver productos clientes (ya tiene los filtros por categoria y disponibilidad)
+app.get("/productosPorCategoria", (req, res) => {
+  const { categoria } = req.query;
+  const sqlSelect = "CALL ObtenerProductosPorCategoria(?);";
+  db.query(sqlSelect, [categoria || null], (err, result) => {
+    if (err) console.log(err);
+    else res.send(result[0]);
+  });
+});
+
+//crear un pedido
+app.post("/addPedido", (req, res) => {
+  const { usuarioId, productos } = req.body;
+  const sqlInsert = "CALL CrearPedido(?, ?);";
+  db.query(sqlInsert, [usuarioId, JSON.stringify(productos)], (err, result) => {
+    if (err) console.log(err);
+    else res.send("Pedido registrado");
+  });
+});
+
+//ver productos de un pedido 
+app.get("/pedidoProductos/:pedidoId", (req, res) => {
+  const pedidoId = req.params.pedidoId;
+  const sqlSelect = "CALL ObtenerPedidoProductosPorID(?);";
+  db.query(sqlSelect, [pedidoId], (err, result) => {
+    if (err) console.log(err);
+    else res.send(result[0]);
+  });
+});
+
+// agregar un producto al carrito
+app.post("/addProductoAlCarrito", (req, res) => {
+  const { usuarioId, productoId, cantidad } = req.body;
+  const sqlInsert = "CALL AgregarProductoAlCarrito(?, ?, ?);";
+  db.query(sqlInsert, [usuarioId, productoId, cantidad], (err, result) => {
+    if (err) console.log(err);
+    else res.send("Producto agregado al carrito");
+  });
+});
+
+app.get("/carrito/:usuarioId", (req, res) => {
+  const usuarioId = req.params.usuarioId;
+  const sqlCall = "CALL ObtenerProductosCarrito(?);";
+
+  db.query(sqlCall, [usuarioId], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error al obtener productos del carrito");
+    } else {
+      res.send(result[0]);
+    }
+  });
+});
+
+app.delete("/carrito/eliminarProducto", (req, res) => {
+  const { usuarioId, productoId } = req.body;
+  const sqlCall = "CALL EliminarProductoDelCarrito(?, ?);";
+
+  db.query(sqlCall, [usuarioId, productoId], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error al eliminar el producto del carrito");
+    } else {
+      res.send("Producto eliminado del carrito");
+    }
+  });
+});
+
+app.post("/editarEstadoReserva", (req, res) => {
+  const { reservaId, nuevoEstado } = req.body;
+  const sqlCall = "CALL EditarEstadoReserva(?, ?);";
+
+  db.query(sqlCall, [reservaId, nuevoEstado], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error al editar el estado de la reserva");
+    } else {
+      res.send(`Estado de la reserva ${reservaId} actualizado a ${nuevoEstado}`);
+    }
+  });
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Conectado a la base de datos");
+
+  //verificar o crear administrador
+  verificarOCrearAdmin();
 });
 
 app.listen(3001, () => {
